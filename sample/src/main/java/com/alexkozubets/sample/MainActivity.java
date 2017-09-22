c05d65f677df4ea7457fec9a3c717b14a022cb89
Alexander <kozubets.alexander@gmail.com>
2017-09-22 23:17:33 +0300

    Create project

---
+++ sample/src/main/java/com/alexkozubets/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alexkozubets.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.alexkozubets.android.utils.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

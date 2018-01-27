764127f8f6ce8b38c5d89f618dbc2340818503e0
Alexander <kozubets.alexander@gmail.com>
2018-01-27 14:22:53 +0200

    Update simple resizing sample - resize view height and keep it square

--- sample/src/main/java/com/alexkozubets/sample/SimpleResizeFragment.java
+++ sample/src/main/java/com/alexkozubets/sample/SimpleResizeFragment.java
@@ -13,13 +13,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.alexkozubets.android.utils.ViewParams;
 import com.alexkozubets.samlpe.R;
 
-public class SimpleResizeFragment extends Fragment implements SeekBar.OnSeekBarChangeListener {
+public class SimpleResizeFragment extends Fragment implements SeekBar.OnSeekBarChangeListener, RadioGroup.OnCheckedChangeListener {
 
     protected static final float MIN = 0.25f;
 
@@ -41,13 +42,15 @@ public class SimpleResizeFragment extends Fragment implements SeekBar.OnSeekBarC
         MAX_HEIGHT = (int) (screen.heightPixels * MAX);
     }
 
-    ImageView imgDroid;
+    private ImageView imgDroid;
 
-    TextView tvDroidSize;
+    private TextView tvDroidSize;
 
-    SeekBar sbWidth;
+    private SeekBar sbSize;
 
-    SeekBar sbHeight;
+    private RadioGroup rgModes;
+
+    private Mode mode;
 
     public SimpleResizeFragment() {
         // Required empty public constructor
@@ -67,11 +70,49 @@ public class SimpleResizeFragment extends Fragment implements SeekBar.OnSeekBarC
     }
 
     @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        switch (checkedId) {
+            case R.id.rb_absolute: {
+                mode = Mode.ABSOLUTE;
+            } break;
+
+            case R.id.rb_relative_to_parent: {
+                mode = Mode.RELATIVE_TO_PARENT;
+            } break;
+
+            case R.id.rb_relative_to_screen: {
+                mode = Mode.RELATIVE_TO_SCREEN;
+            } break;
+        }
+    }
+
+    @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         if (fromUser) {
-            int w = (int) (MIN_WIDTH + percentToFloat(progress) * (MAX_WIDTH - MIN_WIDTH));
-            int h = (int) (MIN_HEIGHT + percentToFloat(progress) * (MAX_HEIGHT - MIN_HEIGHT));
-            ViewParams.of(imgDroid).width(w).height(h).apply();
+            switch (mode) {
+                case ABSOLUTE: {
+//                    int w = (int) (MIN_WIDTH + percentToFloat(progress) * (MAX_WIDTH - MIN_WIDTH));
+                    int h = (int) (MIN_HEIGHT + percentToFloat(progress) * (MAX_HEIGHT - MIN_HEIGHT));
+                    ViewParams.of(imgDroid)
+                            .height(h)
+                            .widthToHeightRatio(1f)
+                            .apply();
+                } break;
+
+                case RELATIVE_TO_PARENT: {
+                    ViewParams.of(imgDroid)
+                            .partOfParentHeight(percentToFloat(progress))
+                            .widthToHeightRatio(1f)
+                            .apply();
+                } break;
+
+                case RELATIVE_TO_SCREEN: {
+                    ViewParams.of(imgDroid)
+                            .partOfScreenHeight(percentToFloat(progress))
+                            .widthToHeightRatio(1f)
+                            .apply();
+                } break;
+            }
         }
     }
 
@@ -86,15 +127,22 @@ public class SimpleResizeFragment extends Fragment implements SeekBar.OnSeekBarC
     }
 
     protected void bindViews() {
+        rgModes = findView(R.id.rg_modes);
         imgDroid = findView(R.id.img_droid);
         tvDroidSize = findView(R.id.tv_droid_size);
-        sbWidth = findView(R.id.sb_width);
-        sbHeight = findView(R.id.sb_height);
+        sbSize = findView(R.id.sb_size);
     }
 
     protected void initViews() {
-        sbWidth.setOnSeekBarChangeListener(this);
-        sbHeight.setOnSeekBarChangeListener(this);
+        rgModes.setOnCheckedChangeListener(this);
+        rgModes.check(R.id.rb_absolute);
+        sbSize.setOnSeekBarChangeListener(this);
+        imgDroid.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                tvDroidSize.setText(String.format("W: %d; H: %d", v.getWidth(), v.getHeight()));
+            }
+        });
     }
 
     @FloatRange(from = 0f, to = 1f)
@@ -102,11 +150,13 @@ public class SimpleResizeFragment extends Fragment implements SeekBar.OnSeekBarC
         return percent / 100f;
     }
 
-    protected <T extends Comparable<T>> T clamp(T value, T min, T max) {
-        return value.compareTo(min) < 0 ? min : (value.compareTo(max) > 0 ? max : value);
-    }
-
     protected <T extends View> T findView(@IdRes int id) {
         return getView() != null ? (T) getView().findViewById(id) : null;
+    }
+
+    private enum Mode {
+        ABSOLUTE,
+        RELATIVE_TO_PARENT,
+        RELATIVE_TO_SCREEN,
     }
 }
